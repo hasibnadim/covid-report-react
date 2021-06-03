c11277f9f263b6e1030d76758b719eb1b4e1c070
hasibnadim <nadimshah206@gmail.com>
2021-06-03 12:54:17 +0600

    footre added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Body from './Comp/Body/Body';
+import Footer from './Comp/Footer/Footer';
 import Header from './Comp/Header/Header.jsx';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Body></Body>
+      <Footer></Footer>
     </div>
   );
 }
